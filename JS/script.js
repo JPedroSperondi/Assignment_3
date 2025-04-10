5332dcfa6165e194256a620972e3115be968cbcc
sdhxwan <sumit.dhawxn@gmail.com>
2025-04-10 13:21:14 -0400

    show th epopular gifs even without searching anything so it does not look blank at first

--- JS/script.js
+++ JS/script.js
@@ -32,3 +32,20 @@ async function getAdvice() {
   function toggleDarkMode() {
     document.body.classList.toggle("dark-mode");
   }
+
+  window.onload = async function () {
+  const url = `https://api.giphy.com/v1/gifs/trending?api_key=${apiKey}&limit=10&rating=g`;
+  const response = await fetch(url);
+  const result = await response.json();
+  displayGIFs(result.data);
+};
+
+function displayGIFs(gifs) {
+  const gifContainer = document.getElementById("gifContainer");
+  gifContainer.innerHTML = "";
+  gifs.forEach(gif => {
+    const img = document.createElement("img");
+    img.src = gif.images.fixed_height.url;
+    gifContainer.appendChild(img);
+  });
+}
